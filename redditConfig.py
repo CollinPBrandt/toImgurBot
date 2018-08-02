d397466a332f164035ecccfbdcfeab40886bd42d
Collin <Collin.brandt1@gmail.com>
2018-08-02 14:24:25 -0500

    redditConfig changes for new bot

--- redditConfig.py
+++ redditConfig.py
@@ -1,8 +1,3 @@
-#username = "HeL0BoT"
-#password = "passwordforhellobot"
-#client_id = "cnyIXnsumOBttg"
-#client_secret = "MoY1XBMXJgbqRCATkRhUzhHHwBc"
-
 username = "toImgurBot"
 password = "toImgurBotPassword"
 client_id = "YDIrkAm30-ARIA"
